a1fe22c9bebbecbce6d014191e5e166b6b85de5d
FTEPXW@stud.uni-obuda.hu <FTEPXW@stud.uni-obuda.hu>
2024-03-13 16:30:53 +0100

    test sdded but not finidhed

---
+++ ProductBackendAPI/Tests/ProductControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductBackendAPI.Controllers;
+using ProductBackendAPI.Models;
+
+namespace ProductBackendAPI.Tests
+{
+    public class ProductControllerTests
+    {
+        [Fact]
+        public void Get_ReturnsListOfProducts()
+        {
+            // Arrange
+            var mockLogger = new Mock<ILogger<ProductController>>();
+            var controller = new ProductController(mockLogger.Object);
+
+            // Act
+            var result = controller.Get();
+
+            // Assert
+            Assert.IsType<List<Product>>(result);
+        }
+
+        [Fact]
+        public void Get_ReturnsNotFoundForNonexistentId()
+        {
+            // Arrange
+            var mockLogger = new Mock<ILogger<ProductController>>();
+            var controller = new ProductController(mockLogger.Object);
+
+            // Act
+            var result = controller.Get("nonexistentId");
+
+            // Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+    }
+}
+
